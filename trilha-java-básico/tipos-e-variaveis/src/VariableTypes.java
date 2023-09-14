328d47dea2ad47ee434a1091567875c0be0c8a31
enzzodani <gomes.enzodani@gmail.com>
2023-09-14 12:55:15 -0300

    Variable and Types java class

---
+++ trilha-java-básico/tipos-e-variaveis/src/VariableTypes.java
@@ -0,0 +1,17 @@
+@SuppressWarnings("ALL")
+public class VariableTypes {
+    public static void main(String[] args) {
+        double minimumWage = 2500;
+        String myName = "Enzo Daniel Marinho";
+
+        short shortNumber = 1;
+        int normalNumber = shortNumber;
+        short shortNumber2 = (short) normalNumber;
+
+        //Variables and Constants
+        int number = 5;
+        number = 10;
+
+        final double PI_VALUE = 3.14; //Value never change
+    }
+}
