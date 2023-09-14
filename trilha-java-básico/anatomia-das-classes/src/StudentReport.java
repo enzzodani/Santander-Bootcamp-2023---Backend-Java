9e73b3575bf2937bdbe013f84ef464c289eb2773
enzzodani <gomes.enzodani@gmail.com>
2023-09-14 12:21:46 -0300

    New Class Java

---
+++ trilha-java-básico/anatomia-das-classes/src/StudentReport.java
@@ -0,0 +1,12 @@
+@SuppressWarnings("ALL")
+public class StudentReport {
+    public static void main(String[] args) {
+        int finalMedia = 7;
+            if (finalMedia < 6)
+                System.out.println("DISAPPROVED");
+            else if (finalMedia == 6)
+                System.out.println("MINERVA TEST");
+            else
+                System.out.println("APPROVED");
+        }
+    }
