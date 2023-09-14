446d62a927c4b2f5939db2c228f981dd41b24423
enzzodani <gomes.enzodani@gmail.com>
2023-09-14 14:37:02 -0300

    Operators class java

---
+++ trilha-java-básico/operadores/src/Operators.java
@@ -0,0 +1,22 @@
+import java.sql.Date;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        /* class Operators.java */
+            String name = "Enzo";
+            int age = 19;
+            double high = 1.73;
+            char gender = 'M';
+            boolean student = true;
+
+        /* classe Operators.java */
+            double sum = 10.5 + 15.7;
+            int diff = 113 - 25;
+            int times = 20 * 7;
+            int divide = 15 / 3;
+            int mod = 18 % 3;
+            double result = (10 * 7) + (20/4);
+
+    }
+}
---
+++ trilha-java-básico/operadores/src/debug.java
@@ -0,0 +1,22 @@
+public class debug {
+    public static void main(String[] args) {
+
+    //qual o resultado das expressoes abaixo?
+            String concatenacao ="?";
+
+            concatenacao = 1+1+1+"1";
+            System.out.println(concatenacao);
+
+            concatenacao = 1+"1"+1+1;
+            System.out.println(concatenacao);
+
+            concatenacao = 1+"1"+1+"1";
+            System.out.println(concatenacao);
+
+            concatenacao = "1"+1+1+1;
+            System.out.println(concatenacao);
+
+            concatenacao = "1"+(1+1+1);
+            System.out.println(concatenacao);
+    }
+}
